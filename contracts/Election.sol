a6fd8a454766fc7e2e30070e3d93fe6b4acdc7bb
Aarthi0705 <aarthimanoharan2003@gmail.com>
2023-02-19 11:14:19 +0530

    List Candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,10 +2,27 @@ pragma solidity >=0.4.16 <0.9.0;
 
 contract Election{
 
-    string public candidate;
+    // string public candidate; --smoke test
+    struct Candidate{
+        uint id;
+        string name;
+        uint voteCount;
+    }
 
-    function election( ) public{
-        candidate = "Candidat 1";
+    mapping(uint => Candidate)public candidates;//fetch
+
+    uint public candidatesCount;//store count
+
+    constructor() public{
+        // candidate = "Candidat 1"; --smoke test
+        addCandidate("Candidate 1");
+        addCandidate("Candi 2");
 
     }
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+
+    }
+
 }
